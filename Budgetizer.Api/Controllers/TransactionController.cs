82f01a8a229cad28d03aaefc041bbd82959751ae
oscarsandberg92 <sandoscar92@gmail.com>
2024-03-07 21:26:05 +0100

    added controller for transactions with methods for get and post

--- Budgetizer.Api/Controllers/TransactionController.cs
+++ Budgetizer.Api/Controllers/TransactionController.cs
@@ -1,6 +1,50 @@
-﻿namespace Budgetizer.Api.Controllers
+﻿using Budgetizer.Entities;
+using Budgetizer.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Budgetizer.Api.Controllers
 {
-    public class TransactionController
+    [ApiController]
+    [Route("api/transactions")]
+    public class TransactionController: ControllerBase
     {
+        private readonly ITransactionRepository _transactionRepository;
+
+        public TransactionController(ITransactionRepository transactionRepository)
+        {
+            _transactionRepository = transactionRepository;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Transaction>> AddTransaction(Transaction transaction)
+        {
+            var result = await _transactionRepository.AddTransaction(transaction);
+
+            return result;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Transaction>>> GetTransactions()
+        {
+            var result = await _transactionRepository.GetTransactions();
+
+            return result;
+        }
+
+        [HttpGet("{transactionId}")]
+        public async Task<ActionResult<Transaction>> GetTransaction(int transactionId)
+        {
+            var result = await _transactionRepository.GetTransaction(transactionId);
+            if (result is null) return NotFound();
+            return result;
+        }
+
+        [HttpDelete("{transactionId}")]
+        public async Task<ActionResult> DeleteTransaction(int transactionId)
+        {
+            var result = await _transactionRepository.DeleteTransaction(transactionId);
+
+            return result ? NotFound() : BadRequest("Not found");
+        }
     }
 }
