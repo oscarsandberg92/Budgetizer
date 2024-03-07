c49b399e32af3f669383d0df6fdd00242407513f
oscarsandberg92 <sandoscar92@gmail.com>
2024-03-07 21:23:12 +0100

    added ef core and a dbcontext

---
+++ Budgetizer.Infrastructure/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Budgetizer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Budgetizer.Infrastructure.Data
+{
+    public class DataContext : DbContext
+    {
+        public virtual DbSet<Transaction> Transactions { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+            
+        }
+
+        public DataContext() { } // Empty constructor needed for testing
+    }
+}
