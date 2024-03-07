1c63659fd3dc4c0f2690b2a8eaf14d16916b76b7
oscarsandberg92 <sandoscar92@gmail.com>
2024-03-07 21:24:50 +0100

    Added cors, dbcontext and a transient to program.cs

--- Budgetizer.Api/Program.cs
+++ Budgetizer.Api/Program.cs
@@ -1,12 +1,29 @@
+using Budgetizer.Infrastructure.Data;
+using Budgetizer.Infrastructure.Interfaces;
+using Budgetizer.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(cors =>
+{
+    cors.AddPolicy("Dev", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddTransient<ITransactionRepository, TransactionRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +36,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("Dev");
 
 app.MapControllers();
 
